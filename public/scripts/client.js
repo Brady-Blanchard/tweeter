86bbcc601ed1db424990595994c9fd0a8e6016da
Brady-Blanchard <bradyjames97@hotmail.com>
2023-08-05 14:36:47 -0300

    Implement renderTweets function

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -27,23 +27,40 @@ $(document).ready(function() {
               </footer>
             </article>`
   };
-  
-  // Test / driver code (temporary). Eventually will get this from the server.
-  const tweetData = {
-    "user": {
-      "name": "Newton",
-      "avatars": "https://i.imgur.com/73hZDYK.png",
+
+  const renderTweets = function(tweetsArr) {
+    let $tweet = '';
+    for (const tweet of tweetsArr) {
+      $tweet += createTweetElement(tweet);
+    }
+    return $('.container').append($tweet);
+  };
+
+  // fake data
+  const data = [
+    {
+      "user": {
+        "name": "Newton",
+        "avatars": "https://i.imgur.com/73hZDYK.png"
+        ,
         "handle": "@SirIsaac"
       },
-    "content": {
+      "content": {
         "text": "If I have seen further it is by standing on the shoulders of giants"
       },
-    "created_at": 1461116232227
-  }
-  
-  const $tweet = createTweetElement(tweetData);
-  
-  // Test / driver code (temporary)
-  console.log($tweet); // to see what it looks like
-  $('.container').append($tweet); // to add it to the page so we can make sure it's got all the right elements, classes, etc.
+      "created_at": 1461116232227
+    },
+    {
+      "user": {
+        "name": "Descartes",
+        "avatars": "https://i.imgur.com/nlhLi3I.png",
+        "handle": "@rd" },
+      "content": {
+        "text": "Je pense , donc je suis"
+      },
+      "created_at": 1461113959088
+    }
+  ]
+
+  renderTweets(data);
 });
