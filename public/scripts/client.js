fe2f441675841b9231aeef12f90e28973dacd6bd
Brady-Blanchard <bradyjames97@hotmail.com>
2023-08-06 19:43:35 -0300

    Implement function for fetching tweets

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -3,8 +3,10 @@
  * jQuery is already loaded
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
+
 // lets the DOM load before running JS
 $(document).ready(function() {
+  
   // function that creates a new tweet element based on a tweet object
   const createTweetElement = function(tweetObj) {
     const name = tweetObj.user.name;
@@ -37,41 +39,13 @@ $(document).ready(function() {
     return $('.container').append($tweet);
   };
 
-  // fake tweets array data
-  const data = [
-    {
-      "user": {
-        "name": "Newton",
-        "avatars": "https://i.imgur.com/73hZDYK.png"
-        ,
-        "handle": "@SirIsaac"
-      },
-      "content": {
-        "text": "If I have seen further it is by standing on the shoulders of giants"
-      },
-      "created_at": 1461116232227
-    },
-    {
-      "user": {
-        "name": "Descartes",
-        "avatars": "https://i.imgur.com/nlhLi3I.png",
-        "handle": "@rd" },
-      "content": {
-        "text": "Je pense , donc je suis"
-      },
-      "created_at": 1461113959088
-    }
-  ]
-
-  renderTweets(data);
-
   // event listener for new tweets form
   $("form").on("submit", function(event) {
     event.preventDefault();
       const serializedData = $(this).serialize();
       $.ajax({
-        type: "POST",
         url: "/tweets",
+        method: "POST",
         data: serializedData,
         success: function() {
           console.log(serializedData)
@@ -81,4 +55,21 @@ $(document).ready(function() {
         }
       })
   })
+
+  // function for fetching tweets from the http://localhost:8080/tweets page
+  const loadTweets = function() {
+    $.ajax({
+      url: "/tweets",
+      method: "GET",
+      success: function(res) {
+        renderTweets(res);
+        console.log(res)
+      },
+      error: function(err) {
+
+      }
+    })
+  }
+
+  loadTweets();
 });
